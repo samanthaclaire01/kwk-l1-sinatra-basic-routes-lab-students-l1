039da6d0cd234705a3161544940de176841ad15d
Samantha Cassity <cassityclaire@gmail.com>
2018-06-12 21:33:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
-require 'Sinatra'
+require 'sinatra'
 
 class App < Sinatra::Base
-  
+get '/' do
+  "Hello World!"
+end 
+get '/name' do
+  "Sam"
+end
+get '/hometown' do
+  "My hometown is Phoenix"
+end
+get '/favorite song' do
+  "My favcorite song is lithium"
+end 
 end
